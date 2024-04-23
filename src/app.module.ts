2af5ec3e69d9cc30342be1cf8c318958b2df3f3b
J-hoplin1 <jhoplin7259@gmail.com>
2024-04-24 02:10:54 +0900

    Feat: class-validator, class-transformer, config module, swagger 설치 및 ConfigModule 기본 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
