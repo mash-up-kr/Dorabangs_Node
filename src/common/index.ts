44afb09ea8578e586db51829d17860def59b7b37
J-hoplin1 <jhoplin7259@gmail.com>
2024-06-17 23:32:28 +0900

    Feature: ICommonResponse타입에 호환되는 Base Response Interceptor 추가

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
 export * from './error';
 export * from './filter';
+export * from './interceptor';
---
+++ src/common/interceptor/response.interceptor.ts
@@ -0,0 +1,20 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { ICommonResponse } from '../types/type';
+
+export class CommonResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((payload = {}): ICommonResponse => {
+        return {
+          success: true,
+          error: null,
+          data: payload,
+        };
+      }),
+    );
+  }
+}
