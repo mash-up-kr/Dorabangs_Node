ab9a4a9090fdf11d3b8ee20e659608189fe0d570
J-Hoplin <hoplin.dev@gmail.com>
2024-06-28 22:12:32 +0900

    Docs: GetUser 임시 주석 추가

--- src/common/decorators/getUser.decorator.ts
+++ src/common/decorators/getUser.decorator.ts
@@ -2,6 +2,13 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { ReqUserPayload } from '../types/type';
 import { Request } from 'express';
 
+/**
+ * @GetUser
+ *
+ * - Param Decorator
+ * - @GetUser의 매개변수로는 ReqUserPaylod의 프로퍼티를 주거나 생략할 수 있습니다.
+ *
+ */
 export const GetUser = createParamDecorator(
   (
     property: keyof ReqUserPayload,
