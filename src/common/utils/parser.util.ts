533cb9ff87c4e5d219d42e7eb0bfa909257d6ed5
J-hoplin1 <jhoplin7259@gmail.com>
2024-06-29 20:24:39 +0900

    Feat: parseURL utility function 추가

---
+++ src/common/utils/parser.util.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+/**
+ *
+ * HTML Parser Utility Function.
+ *
+ * Parse Content and Title
+ */
+export async function parseURL(url: string): Promise<{
+  title: string;
+  content: string;
+}> {
+  const request = await axios.get(url);
+  const html = request.data;
+  const $ = cheerio.load(html);
+  // HTML Element의 title
+  const title = $('title').text();
+
+  // HTML Body내에 있는 script태그랑 css style태그 제거
+  $('body script, body style').remove();
+  // HTML Element의 body의 text content
+  const content = $('body')
+    .text()
+    .replace(/[\n\t\r]+/g, ' ')
+    .replace(/<\/?[^>]+(>|$)/g, '')
+    .trim();
+  return {
+    title,
+    content,
+  };
+}
