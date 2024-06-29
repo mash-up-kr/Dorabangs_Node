6470fcf8b13625b10be33a23d443839b6b23469b
Jonghun An <jonghun.an@bucketstore.com>
2024-06-30 03:07:58 +0900

    comment: aws lambda client credentials

--- src/infrastructure/aws-lambda/aws-lambda.service.ts
+++ src/infrastructure/aws-lambda/aws-lambda.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InvokeCommand, LambdaClient, LogType } from '@aws-sdk/client-lambda';
 @Injectable()
 export class AwsLambdaService {
+  // TODO  credentials 추가 필요 (acccess_key, secret_key)
   readonly client = new LambdaClient({ region: 'ap-northeast-2' });
 
   invoke_lambda(lambdaFunctionName: string, payload: Object): void {
