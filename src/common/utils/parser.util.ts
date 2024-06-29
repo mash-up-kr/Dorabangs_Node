44000e0dd7801c934d9a9ff23dc2913cce1a2be0
J-hoplin1 <jhoplin7259@gmail.com>
2024-06-29 23:00:18 +0900

    Feat: Page Thumbnail metadata에서 파싱하도록 추가

--- src/common/utils/parser.util.ts
+++ src/common/utils/parser.util.ts
@@ -9,13 +9,17 @@ import * as cheerio from 'cheerio';
 export async function parseLinkTitleAndContent(url: string): Promise<{
   title: string;
   content: string;
+  thumbnail: string;
 }> {
+  // HTML Parsing
   const fetchTest = await fetch(url);
   const fetchHTML = await fetchTest.text();
+  // HTML Cheerio Instance로 변환
   const $ = cheerio.load(fetchHTML);
   // HTML Element의 title
   const title = $('title').text();
-
+  // Page Thumbnail Parsing
+  const thumbnail = $('meta[property="og:image"]').attr('content');
   // HTML Body내에 있는 script태그랑 css style태그 제거
   $('body script, body style').remove();
   // HTML Element의 body의 text content
@@ -25,7 +29,8 @@ export async function parseLinkTitleAndContent(url: string): Promise<{
     .replace(/<\/?[^>]+(>|$)/g, '')
     .trim();
   return {
-    title,
+    title: title ?? '',
     content,
+    thumbnail: thumbnail ?? '',
   };
 }
