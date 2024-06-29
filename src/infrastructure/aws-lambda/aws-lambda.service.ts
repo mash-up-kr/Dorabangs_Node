ab30607819270166231adcac7dd230876c2a3175
Jonghun An <jonghun.an@bucketstore.com>
2024-06-30 02:49:01 +0900

    feat: call lambda invoke

--- src/infrastructure/aws-lambda/aws-lambda.service.ts
+++ src/infrastructure/aws-lambda/aws-lambda.service.ts
@@ -3,4 +3,13 @@ import { InvokeCommand, LambdaClient, LogType } from '@aws-sdk/client-lambda';
 @Injectable()
 export class AwsLambdaService {
   readonly client = new LambdaClient({ region: 'ap-northeast-2' });
+
+  invoke_lambda(lambdaFunctionName: string, payload: Object): void {
+    const command = new InvokeCommand({
+      FunctionName: lambdaFunctionName,
+      InvocationType: 'Event',
+      Payload: JSON.stringify(payload),
+    });
+    this.client.send(command);
+  }
 }
