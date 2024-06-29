cb279f7571be52dfedc717d26d55bda0a5e2e07a
geonoing <jkl9510357@gmail.com>
2024-06-29 15:59:19 +0900

    chore: apply path alias

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schema';
+import { User, UserSchema } from '@src/schema';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './guards/strategy';
 
