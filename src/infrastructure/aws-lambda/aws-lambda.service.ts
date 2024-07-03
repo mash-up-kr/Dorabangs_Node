2bd3819e17c01b0ae89772b0b1cd9e969dc3df5e
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2024-07-03 23:29:28 +0900

    feat: aws lambda credentials

--- src/infrastructure/aws-lambda/aws-lambda.service.ts
+++ src/infrastructure/aws-lambda/aws-lambda.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { InvokeCommand, LambdaClient, LogType } from '@aws-sdk/client-lambda';
+import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AwsLambdaService {
+  constructor(private readonly config: ConfigService) {}
   // TODO  credentials 추가 필요 (acccess_key, secret_key)
-  readonly client = new LambdaClient({ region: 'ap-northeast-2' });
+  readonly client = new LambdaClient({
+    region: 'ap-northeast-2',
+    credentials: {
+      accessKeyId: this.config.get<string>('AWS_ACCESS_KEY'),
+      secretAccessKey: this.config.get<string>('AWS_SECRET_KEY'),
+    },
+  });
 
-  invoke_lambda(lambdaFunctionName: string, payload: Object): void {
+  invoke_lambda(lambdaFunctionName: string, payload: object): void {
     const command = new InvokeCommand({
       FunctionName: lambdaFunctionName,
       InvocationType: 'Event',
