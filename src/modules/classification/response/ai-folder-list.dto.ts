2e6ef65da3f1a355a702c18cf6cb9c87c9058128
hye-on <ain0103@naver.com>
2024-07-05 17:31:12 +0900

    Chore : classification 폴더 이름 리스트 response에 totalCounts(분류한 링크 개수) 추가

--- src/modules/classification/response/ai-folder-list.dto.ts
+++ src/modules/classification/response/ai-folder-list.dto.ts
@@ -4,11 +4,17 @@ import { ClassificationFolderWithCount } from '../dto/classification.dto';
 
 export class AIFolderNameListResponse {
   @ApiProperty({
+    description: 'ai로 분류된 링크의 총 개수',
+  })
+  totalCounts: number;
+
+  @ApiProperty({
     isArray: true,
   })
   list: ClassificationFolderWithCount[];
 
   constructor(data: ClassificationFolderWithCount[]) {
+    this.totalCounts = data.reduce((sum, folder) => sum + folder.postCount, 0);
     this.list = data;
   }
 }
