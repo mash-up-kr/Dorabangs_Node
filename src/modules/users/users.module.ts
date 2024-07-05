01e8d4504da983ef88d24d1162c60527d2d9188a
Phil <phil@classum.com>
2024-07-06 02:25:28 +0900

    feat: add default folder

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,16 +3,20 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtStrategy } from './guards/strategy';
-import { User, UserSchema } from '@src/infrastructure';
+import { Folder, FolderSchema, User, UserSchema } from '@src/infrastructure';
 import { UsersRepository } from './users.repository';
 import { AuthModule } from '../auth/auth.module';
+import { FolderRepository } from '../folders/folders.repository';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Folder.name, schema: FolderSchema },
+    ]),
   ],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, JwtStrategy],
+  providers: [UsersService, UsersRepository, FolderRepository, JwtStrategy],
 })
 export class UsersModule {}
