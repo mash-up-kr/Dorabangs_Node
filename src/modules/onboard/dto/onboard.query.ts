a0534771d54cd4fe41738a77187623b1f6e44cae
J-hoplin1 <jhoplin7259@gmail.com>
2024-07-15 22:15:44 +0900

    Fix: Fix Onboard API bug

--- src/modules/onboard/dto/onboard.query.ts
+++ src/modules/onboard/dto/onboard.query.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, Min } from 'class-validator';
 
 export class OnBoardQuery {
-  @ApiProperty()
+  @ApiProperty({
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
+  @Type(() => Number)
   @Min(1)
   limit: number;
 }
