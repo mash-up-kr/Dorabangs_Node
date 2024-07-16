25ff070de4620366f3c735e16723ad7d5fef30da
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2024-07-16 23:39:07 +0900

    refactor: erase comment

--- src/ai_handler.ts
+++ src/ai_handler.ts
@@ -4,7 +4,6 @@ import { AiService } from '@src/infrastructure/ai/ai.service';
 import { AppModule } from '@src/app.module';
 
 export const handler: Handler = async (event: any, context: Context) => {
-  // event 데이터 잘 들어오는지 확인용
   const app = await NestFactory.create(AppModule);
   const aiService = app.get(AiService);
 
