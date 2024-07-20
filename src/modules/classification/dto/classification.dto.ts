068f1c48d8b182e47f065e3306714c247249de62
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2024-07-21 01:40:06 +0900

    refactor: swagger definition

--- src/modules/classification/dto/classification.dto.ts
+++ src/modules/classification/dto/classification.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty } from 'class-validator';
+import { PostAiStatus } from '@src/modules/posts/posts.constant';
 
 export interface ClassificationFolderWithCount {
   folderId: string;
@@ -7,22 +8,45 @@ export interface ClassificationFolderWithCount {
   postCount: number;
 }
 
-export interface PostListInClassificationFolder {
+export class ClassificationPostList {
+  @ApiProperty({ required: true, description: '피드 id', type: String })
   postId: string;
 
+  @ApiProperty({ required: true, description: '폴더 id', type: String })
   folderId: string;
 
+  @ApiProperty({ required: true, description: '피드 제목', type: String })
   title: string;
 
+  @ApiProperty({ required: true, description: '피드 URL', type: String })
   url: string;
 
+  @ApiProperty({ required: true, description: '피드 요약', type: String })
   description: string;
 
+  @ApiProperty({
+    required: true,
+    description: '키워드',
+    isArray: true,
+    type: String,
+  })
   keywords: string[];
 
+  @ApiProperty({
+    required: true,
+    description: 'ai 요약 상태',
+    enum: PostAiStatus,
+  })
+  aiStatus: PostAiStatus;
+
+  @ApiProperty({ required: false, description: '피드 og 이미지', type: String })
+  thumbnailImgUrl: string | null;
+
+  @ApiProperty({ required: false, description: '생성 시간', type: Date })
   createdAt: Date;
 
-  readAt: Date;
+  @ApiProperty({ required: false, description: '읽은 시간', type: Date })
+  readAt: Date | null;
 }
 
 export class UpdateAIClassificationDto {
