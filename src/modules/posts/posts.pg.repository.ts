1f3ac14cbea43ac33f7c8261db19aca799384eee
J-Hoplin <jhoplin7259@gmail.com>
2024-07-27 15:55:13 +0900

    Feat: Add pg post listpost and findPostOrThrow method

--- src/modules/posts/posts.pg.repository.ts
+++ src/modules/posts/posts.pg.repository.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { P001 } from './error';
 
 @Injectable()
 export class PostsPGRepository {
@@ -21,5 +23,44 @@ export class PostsPGRepository {
       },
     });
     return userPostCount;
+  }
+
+  async listPost(
+    userId: string,
+    page: number,
+    limit: number,
+    order = Prisma.SortOrder.desc,
+    isFavorite?: boolean,
+    isRead?: boolean,
+  ) {
+    const skip = (page - 1) * limit;
+    const posts = await this.prisma.post.findMany({
+      skip: skip,
+      take: limit,
+      where: {
+        isFavorite: isFavorite ? true : undefined,
+        readAt: isRead
+          ? {
+              not: null,
+            }
+          : undefined,
+      },
+      orderBy: {
+        createdAt: order,
+      },
+    });
+    return posts;
+  }
+
+  async findPostOrThrow(param: Prisma.PostWhereUniqueInput) {
+    const post = await this.prisma.post.findUnique({
+      where: {
+        ...param,
+      },
+    });
+    if (!post) {
+      throw new NotFoundException(P001);
+    }
+    return post;
   }
 }
