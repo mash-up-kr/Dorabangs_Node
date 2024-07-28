ab251e17328827df8207850726b364e9c2cc6582
J-hoplin1 <jhoplin7259@gmail.com>
2024-07-29 01:31:46 +0900

    Fix: Temporary pg repository, prisma annotate

--- src/modules/users/users.pg.repository.ts
+++ src/modules/users/users.pg.repository.ts
@@ -1,30 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+// import { Injectable } from '@nestjs/common';
+// import { PrismaService } from '../prisma/prisma.service';
 
-// TODO: Service class name will be changed after the naming convention is discussed later.
-@Injectable()
-export class UsersPGRepository {
-  constructor(private readonly prisma: PrismaService) {}
-  private defaultFolderName = '나중에 읽을 링크';
+// // TODO: Service class name will be changed after the naming convention is discussed later.
+// @Injectable()
+// export class UsersPGRepository {
+//   constructor(private readonly prisma: PrismaService) {}
+//   private defaultFolderName = '나중에 읽을 링크';
 
-  async findOrCreate(deviceToken: string) {
-    const user = await this.prisma.user.upsert({
-      where: {
-        deviceToken: deviceToken,
-      },
-      create: {
-        deviceToken: deviceToken,
-        folders: {
-          create: [
-            {
-              name: this.defaultFolderName,
-              type: 'default',
-            },
-          ],
-        },
-      },
-      update: {},
-    });
-    return user;
-  }
-}
+//   async findOrCreate(deviceToken: string) {
+//     const user = await this.prisma.user.upsert({
+//       where: {
+//         deviceToken: deviceToken,
+//       },
+//       create: {
+//         deviceToken: deviceToken,
+//         folders: {
+//           create: [
+//             {
+//               name: this.defaultFolderName,
+//               type: 'default',
+//             },
+//           ],
+//         },
+//       },
+//       update: {},
+//     });
+//     return user;
+//   }
+// }
