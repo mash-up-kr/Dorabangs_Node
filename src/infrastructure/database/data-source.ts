88aba4dc7433107a3829e1273e08b637aedb16e7
Ahn-seokjoo <tjrwn0716@naver.com>
2025-02-15 22:39:22 +0900

    fix: 빌드 실패 해결

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -1,11 +1,12 @@
+import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 
-require('dotenv').config();
+dotenv.config();
 
 export const dbConfig: any = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
